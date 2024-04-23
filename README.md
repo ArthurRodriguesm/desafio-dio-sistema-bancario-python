a72465318d0c280ec0badcdd5ce6759211f94d61
arthurrodriguesm <arthurmartins010203@gmail.com>
2024-04-23 16:19:55 -0300

    insercao de metodos 'depositar' e 'sacar'

---
+++ README.md
@@ -0,0 +1,18 @@
+# DESAFIO DIO - Criando um Sistema Bancário com Python
+
+## Proposta
+Criar uma simulação simples de um sistema bancário onde é possível realizar Saque, Depósito e exibir Extrato.
+
+### Condições
+- Realizar apenas 3 saques por dia
+- Valor máximo de saque no dia R$500,00
+- Apenas valores positivos de depósito
+- Todos os depósitos armazenados em uma variável
+- Deve ser exibido o saldo no extrato
+
+## Etapas
+
+- > Criar Menu com opções: Saque, Depósito e Extrato
+- > Criar método de Depósito
+- > Criar método de Saque
+- > Criar método de exibir Extrato
--- bank.py
+++ bank.py
@@ -1,26 +1,51 @@
-def deposito(valor):
-    print(f"Depósito de R$ {valor} feito com sucesso!")
-    
-def saque(valor):
-    print(f"Saque de R$ {valor} feito com sucesso!")
+LIMITE_SAQUE_DIARIO = 3
+LIMITE_VALOR_SAQUE = 500
+saldo = 0
+quantidade_saque = 0
+extrato = '''
+    =================
+         Extrato
+    =================
 
-def MENU():
-    print('''
-    ============
-        MENU
-    ============
+'''
+
+def depositar(saldo, extrato):
+    valor = float(input("Insira o valor do depósito: R$"))
+
+    if valor <= 0:
+        print("Erro")
+        False
+    else:
+        saldo += valor
+        print(f"Depósito de R${valor:.2f} feito com sucesso!\n")
+    print(f"{extrato} Saldo: R${saldo:.2f}")
+
+def sacar():
+    valor = float(input("Insira o valor do depósito: R$"))
+    
+
+menu = '''
+    ==============
+         MENU
+    ==============
     [1] - Depositar
     [2] - Sacar
-    [3] - Extrato Bancário
-''')
-    
-    opcao = int(input("Digite o número da operação desejada: "))
+    [3] - Exibir Extrato
+    [4] - Sair
 
-    if opcao is 1:
-        valor = float(input("Digite o valor que deseja depositar: "))
-        deposito(valor)
-    elif opcao is 2:
-        valor = float(input("Digite o valor que deseja sacar: "))
-        saque(valor)
+    Digite o número da operação desejada:'''
 
-MENU()
+
+while True:
+    opcao = int(input(menu))
+
+    if opcao == 1:
+        depositar(saldo, extrato)
+    elif opcao == 2:
+        sacar()
+    elif opcao == 3:
+        extrato()
+    elif opcao == 4:
+        print("Obrigado! E até mais :)")
+        False
+        break
